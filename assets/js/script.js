62893b4d916cad1938c9ca1881a5747cc42f072f
Dan Norgard <daniel.norgard@gmail.com>
2021-05-03 21:15:02 -0500

    added js for loading random question and display wrong or correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let questionEl = document.querySelector("#question");
 let choicesEl = document.querySelector("#choices");
 let answersEl = document.querySelector("#answers");
+let startBoxEl = document.querySelector("#start-box");
 let startEl = document.querySelector("#start");
 let timerEl = document.querySelector("#countdown");
-let secondsLeft=20;
-let remainingQuestions;
+let secondsLeft=76;
+let gameOver=false
 let correctResponse="";
+let usedQs=[];
 const myQuestion=[
     {
         question: "Who invented JavaScript?",
@@ -31,33 +33,51 @@ const myQuestion=[
 
 // add event listener to start button to start quiz
 startEl.addEventListener("click", function(){
-    remainingQuestions=5;
     // start timer
     countDown();
-    // load the first question and possible answers
-    loadQuestion();
+    // choose and load the first question and possible answers
+    chooseQuestion();
 });
+// listen for an answer button to be clicked.
 choicesEl.addEventListener("click", function(event){
     let element = event.target;
     if(element.matches("button")===true){
         let userAnswer = element.getAttribute("answer-options");
+        startBoxEl.style.borderTop="1px solid #000";
         if(userAnswer===correctResponse){
-            questionEl.textContent="Correct"
+            startBoxEl.textContent="Correct!";
         }else{
-            questionEl.textContent="Wrong"
+            startBoxEl.textContent="Wrong!";
+            secondsLeft -= 15;
         }
-
+        if(usedQs.length<2){
+            chooseQuestion();
+        }else{
+            // run highscore function
+            gameOver=true;
+        }
     }
 })
-function loadQuestion(){
-    questionEl.textContent=myQuestion[0].question;
+function chooseQuestion(){
+    let shuffleQs = Math.floor(Math.random()*myQuestion.length);
+    if(usedQs.includes(shuffleQs)){
+        chooseQuestion();
+    }
+    else{
+        usedQs.push(shuffleQs);
+        loadQuestion(shuffleQs);
+    }
+}
+function loadQuestion(q){
+    
+    questionEl.textContent=myQuestion[q].question;
+    choicesEl.textContent="";
     startEl.textContent="";
-    answersEl.textContent="";
-    correctResponse=myQuestion[0].correctAnswer;
+    correctResponse=myQuestion[q].correctAnswer;
     for(i=0; i<4; i++){
         let index = ["a", "b", "c", "d"];
         let button=document.createElement("button");
-        button.textContent=myQuestion[0].answers[index[i]];
+        button.textContent=myQuestion[q].answers[index[i]];
         button.setAttribute("answer-options", index[i]);
         choicesEl.appendChild(button);
     }
@@ -69,20 +89,15 @@ function countDown(){
     let timerInterval=setInterval(function(){
         secondsLeft--;
         timerEl.textContent="time: " + secondsLeft;
-
-        if(secondsLeft===0) {
+        startBoxEl.textContent="";
+        startBoxEl.style.borderTop="none";
+        if(secondsLeft===0 || gameOver) {
             clearInterval(timerInterval);
             // run our highscore function
         }
     },1000);
 }
-answersEl.addEventListener("click", function(event){
-    let element=event.target;
-    if(element.matches("li")){
-        let x = element.getAttribute("answer-id");
-        console.log(x);
-    }
-})
+
 // once answer is selected check if it is right.
     // if right, move onto next question & display correct.
     // if wrong, deduct time from timer, display wrong, move onto next question
